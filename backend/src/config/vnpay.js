577ef87e8bdc7d09b7cecb548c29809cd03d4382
darealest <zozobozza123@gmail.com>
2026-04-25 13:17:32 +0700

    update

--- backend/src/config/vnpay.js
+++ backend/src/config/vnpay.js
@@ -1,6 +1,8 @@
+require('dotenv').config(); // Nhớ require dotenv nếu đang dùng Node.js thường
+
 module.exports = {
-    vnp_TmnCode: "RP54MSHK", // <--- Thay mã TmnCode bạn nhận được vào đây
-    vnp_HashSecret: "TL3FCMW7Z1KME1N1FL2XL80STXJHCF95", // <--- Thay chuỗi HashSecret vào đây
-    vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
-    vnp_ReturnUrl: "http://localhost:5173/payment/result" // Frontend sẽ đón khách ở trang này sau khi thanh toán
-  };
+  vnp_TmnCode: process.env.VNP_TMN_CODE,
+  vnp_HashSecret: process.env.VNP_HASH_SECRET,
+  vnp_Url: process.env.VNP_URL,
+  vnp_ReturnUrl: process.env.VNP_RETURN_URL
+};
